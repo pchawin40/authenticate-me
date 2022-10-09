331baf4f520470889e87213e9fda7029839bc6d5
chawin <pathocha000@gmail.com>
2022-10-08 19:31:57 -0700

    Successfully added and tested error handlers middlewares

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,9 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
+//? import for Sequelize Error-Handler
+const { ValidationError } = require('sequelize');
+
 // Express routes
 const routes = require('./routes');
 
@@ -45,6 +48,39 @@ app.use(
 );
 
 app.use(routes); // Connect all the routes
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+//? Sequelize Error-Handler
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+//? Error formatter
+// for formatting all errors before returning JSON response
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 // export app
 module.exports = app;
